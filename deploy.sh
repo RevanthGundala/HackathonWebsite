44913b67d031b31ed6edf8d2516ecd27599060e6
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-03-11 16:24:58 -0700

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
+# nvm use 21.1
+# echo "git add ."
+# echo "git commit -m 'updates'"
+# nvm use 12.1
+# echo "git push"
+
+
 nvm use 21.1
-echo "git add ."
-echo "git commit -m 'updates'"
+git add .
+git commit -m 'updates'
 nvm use 12.1
-echo "git push"
+git push
